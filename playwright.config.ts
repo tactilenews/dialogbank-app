a1465a57f9d36d51a57cd9a326f0648c70aeb797
Robert Schäfer <git@roschaefer.de>
2026-03-07 11:17:59 +0100

    refactor: forbid 'only' focused playwright tests on CI (#13)

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,5 +3,7 @@ import { defineConfig } from '@playwright/test';
 export default defineConfig({
 	webServer: { command: 'pnpm run build && pnpm run preview', port: 4173 },
 	testDir: '.',
-	testMatch: ['**/*.e2e.spec.ts']
+	testMatch: ['**/*.e2e.spec.ts'],
+	// Fail the build on CI if you accidentally left test.only in the source code.
+	forbidOnly: !!process.env.CI
 });
