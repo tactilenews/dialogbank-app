d282100bcee0aae6b181a2261c2205d05210e47f
Robert Schäfer <git@roschaefer.de>
2026-03-07 01:56:25 +0100

    refactor: local development with neon proxy (#12)

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -1,9 +1,16 @@
 import { drizzle } from 'drizzle-orm/neon-http';
-import { neon } from '@neondatabase/serverless';
+import { neon, neonConfig } from '@neondatabase/serverless';
 import * as schema from './schema';
 import { env } from '$env/dynamic/private';
 
 if (!env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
+
+// Configure for local development/testing with Neon Proxy
+if (env.DATABASE_URL.includes('localhost') || env.DATABASE_URL.includes('127.0.0.1')) {
+	// HTTP Mode (recommended for most applications)
+	neonConfig.fetchEndpoint = 'http://localhost:5432/sql'; // Routes HTTP requests to local proxy
+	neonConfig.poolQueryViaFetch = true; // Enables HTTP connection pooling
+}
 
 const client = neon(env.DATABASE_URL);
 
---
+++ src/scripts/seed.ts
@@ -0,0 +1,27 @@
+import { auth as baseAuth } from '$lib/server/auth';
+import { db } from '$lib/server/db';
+import * as schema from '$lib/server/db/schema';
+import { seed, reset } from 'drizzle-seed';
+import { betterAuth } from 'better-auth/minimal';
+
+const auth = betterAuth({
+	...baseAuth.options,
+	...{
+		emailAndPassword: {
+			enabled: true,
+			disableSignUp: false
+		},
+		plugins: undefined
+	}
+});
+
+await reset(db, schema); // Wipes existing data
+await seed(db, { task: schema.task });
+
+await auth.api.signUpEmail({
+	body: {
+		email: 'user@example.org',
+		password: '12341234',
+		name: 'John Doe'
+	}
+});
