65f18b6ae85a75d3431d0350a6a8a6cda2a40612
Robert Schäfer <git@roschaefer.de>
2026-03-10 13:00:39 +0100

    refactor: drop table task (#25)

--- src/lib/server/db/schema.ts
+++ src/lib/server/db/schema.ts
@@ -1,10 +1,4 @@
-import { pgTable, serial, integer, text } from 'drizzle-orm/pg-core';
-
-export const task = pgTable('task', {
-	id: serial('id').primaryKey(),
-	title: text('title').notNull(),
-	priority: integer('priority').notNull().default(1)
-});
+import { pgTable, serial, text } from 'drizzle-orm/pg-core';
 
 export const answers = pgTable('answers', {
 	id: serial('id').primaryKey(),
